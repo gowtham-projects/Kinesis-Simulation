aa3888c412457fd13ff0fba23863e577593b4411
Suneel Kumar <suneelkumar@Suneels-MacBook-Air.local>
2025-08-11 17:52:07 +0530

    Initial Commit

---
+++ script.py
@@ -0,0 +1,32 @@
+import boto3
+import json
+import time
+import random
+
+stream_name = 'SensorData'
+region_name = 'us-east-1'
+
+kinesi_client = boto3.client('kinesis', region_name=region_name)
+
+def generate_random_data():
+    return {
+        'sensor_id': random.randint(1, 10),
+        'temperature': round(random.uniform(20.0, 30.0), 2),
+        'humidity': round(random.uniform(30.0, 60.0), 2),
+        'timestamp': time.time()
+    }
+def send_data_to_kinesis():
+    while True:
+        data = generate_random_data()
+        partition_key = str(data['sensor_id'])
+        print(f"Sending data: {data}")
+        kinesi_client.put_record(
+            StreamName=stream_name,
+            Data=json.dumps(data),
+            PartitionKey=str(data['sensor_id'])
+        )
+        time.sleep(1)
+
+if __name__ == "__main__":
+    print("Starting to send data to Kinesis stream...")
+    send_data_to_kinesis()
---
+++ test.sql
@@ -0,0 +1,15 @@
+%flink.ssql
+DROP TABLE IF EXISTS sensordata.sensor_data_table;
+
+CREATE TABLE sensor_data_table (
+    sensor_id INT,
+    temperature DOUBLE,
+    humidity DOUBLE,
+    `timestamp` DOUBLE
+) WITH (
+    'connector' = 'kinesis',
+    'stream' = 'SensorData',
+    'aws.region' = 'us-east-1',
+    'scan.stream.initpos' = 'TRIM_HORIZON',
+    'format' = 'json'
+);
